7573b97f066615c398da869a0e3fd3bee5c0139a
Daniel Raymond <dan@raymond.ch>
2023-11-30 23:16:23 -0500

    Added shot optimization

--- bullet.py
+++ bullet.py
@@ -1,8 +1,11 @@
 import pybullet as p
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, LinearConstraint
 from time import sleep
+from tqdm import tqdm
 
+
+np.seterr(invalid='raise')
 NET_RADIUS = 0.4572 # m
 BALL_RADIUS = 0.04445 # m
 NET_MODEL = "net.obj"
@@ -17,11 +20,11 @@ class SpikeBallSimulator:
         self.plot = plot
         p.connect(p.GUI if plot else p.DIRECT)
         # Camera settings
-        if plot and not trimetric:
+        if plot:
             cameraTargetPosition = [0, 0, 0]  # x, y, z
             cameraDistance = 2
-            cameraYaw = np.pi/2
-            cameraPitch = 0
+            cameraYaw = 0
+            cameraPitch = -30 if trimetric else 0
             p.resetDebugVisualizerCamera(cameraDistance, cameraYaw, cameraPitch, cameraTargetPosition)
         p.setGravity(0, 0, -g)
         # Net model
@@ -63,9 +66,10 @@ class SpikeBallSimulator:
             p.createSoftBodyAnchor(self.net, nodeIndex, -1, -1)  # Anchor to a fixed point in space
         self.init_state = p.saveState()
 
-    def run(self, rim_contact_dist, vx, vy, min_steps=4, let_run=False, raise_on_error=True):
+    def run(self, rim_contact_dist, vx, vy, min_steps=4, let_run=False):
         p.restoreState(self.init_state)
-        p.resetBasePositionAndOrientation(self.ball, [NET_RADIUS - rim_contact_dist, 0, BALL_RADIUS], [0, 0, 0, 1])
+        contact_height = BALL_RADIUS + self.contact_margin
+        p.resetBasePositionAndOrientation(self.ball, [NET_RADIUS - rim_contact_dist, 0, contact_height], [0, 0, 0, 1])
         p.resetBaseVelocity(self.ball, linearVelocity=[vx, 0, -vy], angularVelocity=[0, 0, 0])
 
         ball_coords = []
@@ -83,25 +87,29 @@ class SpikeBallSimulator:
             # Get ball position
             (x, y, z), _ = p.getBasePositionAndOrientation(self.ball)
             ball_coords.append((x, y, z))
-            if not sim_started and z < BALL_RADIUS:
+            if not sim_started and z < contact_height:
                 sim_started = True
-            elif not let_run and sim_started and z > BALL_RADIUS and step_count > min_steps:
+            elif not let_run and sim_started and z > contact_height and step_count > min_steps:
+                break
+            elif np.linalg.norm([x, y]) > NET_RADIUS + BALL_RADIUS and not let_run:
                 break
             elif step_count > self.max_steps:
                 if let_run:
                     break
+                if not sim_started:
+                    raise Exception(f"Ball never hit net with input x={rim_contact_dist}, vx={vx}, vy={vy}")
                 raise Exception(f"Simulation timed out with input x={rim_contact_dist}, vx={vx}, vy={vy}")
-            elif z < -3*BALL_RADIUS and raise_on_error:
+            elif z < -3*BALL_RADIUS and not let_run:
                 raise Exception(f"Ball fell through net with input x={rim_contact_dist}, vx={vx}, vy={vy}")
         return np.array(ball_coords) # (step_count, 3)
 
     def get_output_state(self, *input_state, **kwargs) -> tuple[float, float, float]:
-        """Returns the output state (rim_dist, vx_out, vz_out) of the ball given the ball coordinates"""
+        """Returns the output state (rim_dist, vx_out, vy_out) of the ball given the ball coordinates"""
         ball_coords = self.run(*input_state, **kwargs, let_run=False)
         rim_dist = NET_RADIUS - ball_coords[-1, 0]
         v_vec = p.getBaseVelocity(self.ball)[0]
-        vx_out, vz_out = v_vec[0], -v_vec[2]
-        return rim_dist, vx_out, vz_out
+        vx_out, vy_out = v_vec[0], -v_vec[2]
+        return rim_dist, vx_out, vy_out
 
 
 
@@ -112,13 +120,15 @@ def sim_error(correct_out: np.ndarray, sim_out: np.ndarray) -> float:
     return np.sum(np.linalg.norm(correct_out - sim_out, axis=-1))
 
 
-# Optimize material properties of the net to match the output state
+
 def optimize_net_sim(input_states: np.ndarray, output_states: np.ndarray):
-    assert len(input_states) == len(output_states)
+    """Finds the optimal net parameters to match the output states given the input states"""
+    assert input_states.shape == output_states.shape
     assert len(input_states.shape) == 2
 
+    pbar = tqdm()
     def objective(x):
-        print(f"New iteration with mu={x[0]}, lambda={x[1]}")
+        pbar.update()
         sim = SpikeBallSimulator(net_mu=x[0], net_lambda=x[1], plot=False)
         try:
             sim_out = np.array([sim.get_output_state(*input_state, raise_on_error=False) for input_state in input_states])
@@ -130,31 +140,62 @@ def optimize_net_sim(input_states: np.ndarray, output_states: np.ndarray):
     init_lambda = 5
     x0 = np.array([init_mu, init_lambda])
     res = minimize(objective, x0, method='nelder-mead', options={'xatol': 1e-8, 'disp': True})
+    pbar.close()
+    print(res)
+    return res.x
+
+
+def optimize_pocket_shot(net_mu: float, net_lambda: float, max_v=4):
+    # Find the optimal pocket shot, i.e. the shot rebounds at the shallowest angle possible while still clearing the net
+    sim = SpikeBallSimulator(net_mu, net_lambda, plot=False)
+
+    pbar = tqdm()
+    def objective(x):
+        rim_dist, vx, vy = x
+        _, vx_out, _ = sim.get_output_state(rim_dist, vx, vy)
+        # print(f"New iteration with rim_dist={rim_dist:.3f}, vx={vx:.3f}, vy={vy:.3f} -> vx_out={vx_out:.3f}")
+        pbar.update()
+        return vx_out # Want it to bounce as fast as possible in the negative x direction
+
+    # clear_rim_constr = {
+    #     'type': 'ineq',
+    #     'fun': lambda x: NET_RADIUS - x[0]
+    # }
+    rim_dist_constr = LinearConstraint([1, 0, 0], [2*BALL_RADIUS], [NET_RADIUS], keep_feasible=True)
+    vxin_constr = LinearConstraint([0, 1, 0], [0], [max_v], keep_feasible=True) # Shot must be to the right
+    vyin_constr = LinearConstraint([0, 0, 1], [0], [max_v], keep_feasible=True) # Shot must be downward
+    vin_constr = LinearConstraint([0, 1/np.sqrt(2), 1/np.sqrt(2)], [0], [max_v], keep_feasible=True)
+    res = minimize(objective, [3*BALL_RADIUS, 0.5, 0.5], method='trust-constr', constraints=[rim_dist_constr, vxin_constr, vyin_constr, vin_constr], options={'disp': True})
+    pbar.close()
     print(res)
     return res.x
 
 
 if __name__ == "__main__":
-    rim_dist_range = np.linspace(4*BALL_RADIUS, NET_RADIUS, 10)
-    in_vx_range = np.linspace(0.1, 4, 10)
-    in_vy_range = np.linspace(0.1, 4, 10)
-    input_states = np.vstack([rim_dist_range, in_vx_range, in_vy_range]).T
-    output_states = np.array([ # Simulated with net_mu=1050, net_lambda=10
-        [0.17177305, -0.0119, -0.2394],
-        [0.16058951, 0.1142, -0.84351],
-        [0.15880679, 0.0138, -1.36432],
-        [0.15881482, 0.1067, -1.82267],
-        [0.14964238, 0.2735, -2.30739],
-        [0.15231321, 0.2827, -2.79035],
-        [0.17175946, 0.0828, -3.14165],
-        [0.17279976, 0.2647, -3.62030],
-        [0.17296509, 0.6257, -3.96120],
-        [0.28664461, 0.0848, -3.71219],
-    ])
-    net_mu, net_lambda = optimize_net_sim(input_states, output_states)
-    print(net_mu, net_lambda)
+    # rim_dist_range = np.linspace(4*BALL_RADIUS, NET_RADIUS, 10)
+    # in_vx_range = np.linspace(0.1, 4, 10)
+    # in_vy_range = np.linspace(0.1, 4, 10)
+    # input_states = np.vstack([rim_dist_range, in_vx_range, in_vy_range]).T
+    # output_states = np.array([ # Simulated with net_mu=1050, net_lambda=10
+    #     [0.17177305, -0.0119, -0.2394],
+    #     [0.16058951, 0.1142, -0.84351],
+    #     [0.15880679, 0.0138, -1.36432],
+    #     [0.15881482, 0.1067, -1.82267],
+    #     [0.14964238, 0.2735, -2.30739],
+    #     [0.15231321, 0.2827, -2.79035],
+    #     [0.17175946, 0.0828, -3.14165],
+    #     [0.17279976, 0.2647, -3.62030],
+    #     [0.17296509, 0.6257, -3.96120],
+    #     [0.28664461, 0.0848, -3.71219],
+    # ])
+    # net_mu, net_lambda = optimize_net_sim(input_states, output_states)
+    # print(net_mu, net_lambda)
 
-    # sim = SpikeBallSimulator(net_mu=1050, net_lambda=5, plot=True)
+    # sim = SpikeBallSimulator(net_mu=1050, net_lambda=5, plot=False, trimetric=False)
+    # sim.run(0.08923, 4.000, 0.05772, let_run=True)
+    # print(sim.get_output_state(0.08923, 4.000, 0.05772))
     # for state in input_states:
     #     print(state, end=" -> ")
     #     print(sim.get_output_state(*state))
+
+    optimize_pocket_shot(net_mu=1050, net_lambda=5)
