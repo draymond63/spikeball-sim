859aafc33f336b855521b3334ec88c3f4a65b046
Daniel Raymond <dan@raymond.ch>
2023-12-02 22:15:40 -0500

    Added gif saving

--- bullet.py
+++ bullet.py
@@ -1,5 +1,7 @@
-import pybullet as p
 import numpy as np
+import pybullet as p
+import pandas as pd
+from PIL import Image
 from scipy.optimize import minimize, LinearConstraint
 from time import sleep
 from tqdm import tqdm
@@ -14,18 +16,17 @@ NET_EDGE_NODES = 520
 
 
 class SpikeBallSimulator:
-    def __init__(self, *net_params, max_duration=1, g=9.81, plot=False, trimetric=False, **net_kwargs) -> None:
+    def __init__(self, *net_params, max_duration=0.1, g=9.81, plot=False, trimetric=False, **net_kwargs) -> None:
         self.TIMESTEP = 240
         self.max_steps = int(max_duration*self.TIMESTEP)
         self.plot = plot
         p.connect(p.GUI if plot else p.DIRECT)
         # Camera settings
-        if plot:
-            cameraTargetPosition = [0, 0, 0]  # x, y, z
-            cameraDistance = 2
-            cameraYaw = 0
-            cameraPitch = -30 if trimetric else 0
-            p.resetDebugVisualizerCamera(cameraDistance, cameraYaw, cameraPitch, cameraTargetPosition)
+        cameraTargetPosition = [0, 0, 0]  # x, y, z
+        cameraDistance = 1
+        cameraYaw = 0
+        cameraPitch = -30 if trimetric else 0
+        p.resetDebugVisualizerCamera(cameraDistance, cameraYaw, cameraPitch, cameraTargetPosition)
         p.setGravity(0, 0, -g)
         # Net model
         self.contact_margin = 0.1
@@ -66,13 +67,18 @@ class SpikeBallSimulator:
             p.createSoftBodyAnchor(self.net, nodeIndex, -1, -1)  # Anchor to a fixed point in space
         self.init_state = p.saveState()
 
-    def run(self, rim_contact_dist, vx, vy, min_steps=4, let_run=False):
+    def run(self, rim_contact_dist, vx, vy, min_steps=4, save=None, demo=False):
         p.restoreState(self.init_state)
         contact_height = BALL_RADIUS
+        # Start ball from higher up if it's a demo, but so that it contacts the net at the same point
+        if demo:
+            contact_height *= 3*vy
+            rim_contact_dist *= 1.5*vx
         p.resetBasePositionAndOrientation(self.ball, [NET_RADIUS - rim_contact_dist, 0, contact_height], [0, 0, 0, 1])
         p.resetBaseVelocity(self.ball, linearVelocity=[vx, 0, -vy], angularVelocity=[0, 0, 0])
 
         ball_coords = []
+        frames = []
         sim_started = False
         step_count = 0
         if self.plot:
@@ -80,30 +86,36 @@ class SpikeBallSimulator:
         while p.isConnected():
             if self.plot:
                 sleep(0.01)  # Time in seconds.
-                # p.getCameraImage(320,200,renderer=p.ER_BULLET_HARDWARE_OPENGL)
             else:
                 p.stepSimulation()
+            if save is not None:
+                width, height, rgbImg, depthImg, segImg = p.getCameraImage(720, 480, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+                rgb = np.array(rgbImg, dtype=np.uint8)
+                rgb = np.reshape(rgb, (height, width, 4))[:, :, :3]
+                frames.append(Image.fromarray(rgb, 'RGB'))
             step_count += 1
             # Get ball position
             (x, y, z), _ = p.getBasePositionAndOrientation(self.ball)
             ball_coords.append((x, y, z))
             if not sim_started and z < contact_height:
                 sim_started = True
-            elif not let_run and sim_started and z > contact_height and step_count > min_steps:
+            elif not demo and sim_started and z > contact_height and step_count > min_steps:
                 break
-            elif np.linalg.norm([x, y]) > NET_RADIUS + BALL_RADIUS and not let_run:
+            elif np.linalg.norm([x, y]) > NET_RADIUS + BALL_RADIUS and not demo:
                 break
             elif step_count > self.max_steps:
                 if not sim_started:
                     raise Exception(f"Ball never hit net with input x={rim_contact_dist}, vx={vx}, vy={vy}")
                 break
-            elif z < -3*BALL_RADIUS and not let_run:
+            elif z < -3*BALL_RADIUS and not demo:
                 raise Exception(f"Ball fell through net with input x={rim_contact_dist}, vx={vx}, vy={vy}")
+        if save:
+            frames[0].save(save, format='GIF', append_images=frames[1:], save_all=True, duration=len(frames)/self.TIMESTEP/10, loop=0)
         return np.array(ball_coords) # (step_count, 3)
 
     def get_output_state(self, *input_state, **kwargs) -> tuple[float, float, float]:
         """Returns the output state (rim_dist, vx_out, vy_out) of the ball given the ball coordinates"""
-        ball_coords = self.run(*input_state, **kwargs, let_run=False)
+        ball_coords = self.run(*input_state, **kwargs, demo=False)
         rim_dist = NET_RADIUS - ball_coords[-1, 0]
         v_vec = p.getBaseVelocity(self.ball)[0]
         vx_out, vy_out = v_vec[0], -v_vec[2]
